2f16f351793a13c9dc1c5018cb099e007cede0c7
RexTech8 <rexmartin1403@gmail.com>
2022-11-20 20:58:54 -0500

    installed bootstrap theme and other stuff

---
+++ ProjectOne/public/javascripts/scripts.js
@@ -0,0 +1,9 @@
+//IIFE --> immediatley invoked function expression
+(function(){
+    function Start()
+    {
+        console.log("App Started");
+        window.addEventListener("load", Start);
+
+    }
+})();
