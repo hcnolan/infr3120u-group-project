2a68fda2eae8e67ec057363fb621701554ffca7c
hcnolan <112788185+hcnolan@users.noreply.github.com>
2022-11-23 16:04:11 -0500

    Set up mongodb cloud deployment

---
+++ routes/db.js
@@ -0,0 +1,4 @@
+// Defines the URI used in app.js to connect to mongoDB
+module.exports = {
+    "URI": "mongodb+srv://infr:SfbQnDOJUCJyesUQ@groupproject.pzy6aft.mongodb.net/?retryWrites=true&w=majority"
+}
