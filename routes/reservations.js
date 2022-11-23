da1814541e2bfb60f265f1260860bd3c0686282d
hcnolan <112788185+hcnolan@users.noreply.github.com>
2022-11-23 18:20:02 -0500

    Created /reservations page

--- routes/reservations.js
+++ routes/reservations.js
@@ -1,12 +1,21 @@
 let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
+const reservations = require('../models/reservations');
 
 // Connect to model
 let reservationModel = require('../models/reservations')
 
 // Read operation
-
+router.get('/', (req,res,next)=>{
+    reservations.find((err,reservationList)=>{
+        if(err){
+            return console.error(err);
+        } else {
+            res.render('reservations', { title: 'Reservations', reservationList: reservationList });
+        }
+    });
+})
 
 // Add operation
 
