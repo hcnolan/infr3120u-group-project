08189d5eb42a61c6de681e4fd047d4bf39a73248
hcnolan <112788185+hcnolan@users.noreply.github.com>
2022-11-23 16:33:11 -0500

    Created reservations route

--- routes/reservations.js
+++ routes/reservations.js
@@ -1,3 +1,17 @@
 let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
+
+// Connect to model
+let reservationModel = require('../models/reservations')
+
+// Read operation
+
+
+// Add operation
+
+
+// Update operation
+
+
+// Delete operation
