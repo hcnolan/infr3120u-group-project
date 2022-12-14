6adf01a43f18ffc3a8f63ba1752cf1d29f17db70
hcnolan <112788185+hcnolan@users.noreply.github.com>
2022-12-13 22:15:29 -0500

    Tweak azure config

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 // Defines the URI used in app.js to connect to mongoDB
 module.exports = {
-    DATABASE_URL: "mongodb://cars-infr3120u-server:zozvStuVwsYj9Yx5DrXXh82rhI40eW1XGaCtrY6JKfphOhoon2dbwSy6NV33wtAYOwj3Ab2oRpnxACDbNAhHzg==@cars-infr3120u-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@cars-infr3120u-server",
-    DATABASE_NAME: "cars-infr3120u-database"
+    DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_NAME: process.env.DATABASE_NAME
 }
